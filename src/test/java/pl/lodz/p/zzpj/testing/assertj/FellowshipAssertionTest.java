dff6c53c98a6efe19144a4fa5c0cf5ecf86c1b20
Shykial <szymon.jeziorski@interia.pl>
2021-03-29 13:32:44 +0200

    Changed and finished FellowshipAssertionTest

--- src/test/java/pl/lodz/p/zzpj/testing/assertj/FellowshipAssertionTest.java
+++ src/test/java/pl/lodz/p/zzpj/testing/assertj/FellowshipAssertionTest.java
@@ -1,150 +1,103 @@
 package pl.lodz.p.zzpj.testing.assertj;
 
-import org.apache.maven.surefire.shared.lang3.tuple.Triple;
 import org.assertj.core.groups.Tuple;
+import org.assertj.core.util.Streams;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static pl.lodz.p.zzpj.testing.assertj.FellowTestFixture.frodo;
-import static pl.lodz.p.zzpj.testing.assertj.FellowTestFixture.sauron;
 
 public class FellowshipAssertionTest {
 
-    private Fellowship fellowship = formTheFellowshipOfTheRing();
+    private final Fellowship fellowship = formTheFellowshipOfTheRing();
 
     @Test
     public void frodoShouldBeIn() {
-        assertThat(fellowship.toString().contains("Frodo"));
+        assertThat(fellowship.hobbits().stream().
+                anyMatch(hobbit -> hobbit.getName().equalsIgnoreCase("Frodo"))).isTrue();
     }
 
     @Test
     public void sauronShouldNotBeIn() {
-        assertThat(!fellowship.toString().contains("Sauron"));
+        assertThat(Streams.stream(fellowship.iterator())
+                .noneMatch(fellow -> fellow.getName().equalsIgnoreCase("Sauron"))).isTrue();
     }
 
     @Test
     public void shouldFrodoNameBeCorrectAndNotBeASauronAndBeInFellowship() {
-        assertThat(fellowship.toString().contains("Frodo") &&
-                !fellowship.toString().contains("Sauron"));
+        Fellow frodo = frodo();
+        assertThat(frodo.getName().equals("Frodo")
+                && Streams.stream(fellowship.iterator())
+                .anyMatch(hobbit -> hobbit.equals(frodo))).isTrue();
     }
 
     @Test
     public void aragornShouldBeBeforeBoromir() {
-        String[] list = fellowship.toString().split(",");
-        int aragornNum = 0, boromirNum = 0;
-        for (int i = 0; i < list.length; i++) {
-            if (list[i] == "Aragorn") {
-                aragornNum = i;
-            }
-            if (list[i] == "Boromir") {
-                boromirNum = i;
-            }
-        }
-        assertThat((aragornNum < boromirNum));
+        List<String> names = Arrays.asList("Aragorn", "Boromir");
+
+        Iterator<String> names_it = names.iterator();
+
+        assertThat(Streams.stream(fellowship.iterator())
+                .allMatch(
+                        fellow -> !names.contains(fellow.getName())
+                                || fellow.getName().equals(names_it.next())
+                )
+        ).isTrue();
     }
 
     @Test
     public void shouldNotContainDuplicatedFellows() {
-        List<String> list = new ArrayList<>();
-        Iterator<Fellow> iterator = fellowship.iterator();
-        while(iterator.hasNext()) {
-           list.add(iterator.next().getName());
-       }
-        boolean containsDuplicatedFellows = false;
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = 1; j < list.size() ; j++) {
-                if(list.get(i).equals(list.get(j))) {
-                    containsDuplicatedFellows = true;
-                }
-            }
-        }
-        assertThat(containsDuplicatedFellows == false);
+        List<Fellow> fellowList = Streams.stream(fellowship.iterator()).collect(Collectors.toList());
+        Set<Fellow> fellowSet = new HashSet<>(fellowList);
+
+        assertThat(fellowSet).hasSameSizeAs(fellowList);
     }
 
     @Test
     public void shouldContainOnlyFellowsWithExpectedNamesInProperOrder() {
-        List<String> list = new ArrayList<>();
-        boolean[] nations = {false, false, false, false ,false};
-        boolean result = true;
-        Iterator<Fellow> iterator = fellowship.iterator();
-        while(iterator.hasNext()) {
-            String nation = iterator.next().getRace().toString();
-            if(nation == "HOBBIT"){
-                if(nations[0] || nations[1] || nations[2] || nations[3]) {
-                    result = false;
-                }
-            } else if(nation == "MAIAR") {
-                if(nations[0] || nations[1] || nations[2] || nations[3]) {
-                    result = false;
-                }
-                nations = changeBoolean(1, nations);
-            } else if(nation == "ELF") {
-                if(nations[0] || nations[1] || nations[2] || nations[3]) {
-                    result = false;
-                }
-                nations = changeBoolean(2, nations);
-            } else if(nation == "DWARF") {
-                if(nations[0] || nations[1] || nations[2] || nations[3]) {
-                    result = false;
-                }
-                nations = changeBoolean(3, nations);
-            } else if(nation == "MAN") {
-                if(nations[0] || nations[1] || nations[2] || nations[3]) {
-                    result = false;
-                }
-                nations = changeBoolean(4, nations);
-            }
 
-        }
-        assertThat(!result);
+        List<String> expectedNamesList = Arrays.asList("Frodo", "Sam", "Merry", "Pippin", "Gandalf", "Legolas", "Gimli", "Aragorn", "Boromir");
 
-    }
-
-
-    private boolean[] changeBoolean(int i, boolean[] bool) {
-        for(int j = 0; j < i; j++) {
-            bool[j] = true;
-        }
-        return bool;
+        Iterator<String> expectedNamesIt = expectedNamesList.iterator();
+        assertThat(Streams.stream(fellowship.iterator())
+                .count() == expectedNamesList.size() &&
+                Streams.stream(fellowship.iterator())
+                        .allMatch(
+                                fellow -> fellow.getName().equals(expectedNamesIt.next())
+                        )
+        ).isTrue();
     }
 
     @Test
     public void shouldContainNineFellowsButNoneGiant() {
-        boolean isGiant = false;
-        Iterator<Fellow> iterator = fellowship.iterator();
-        int numberOfFellows = 0;
-        while(iterator.hasNext()) {
-            iterator.next();
-            numberOfFellows++;
-        }
-        if(fellowship.toString().contains("GIANT")) {
-            isGiant = true;
-        }
-        assertThat(!isGiant && numberOfFellows == 9);
+        assertThat(Streams.stream(fellowship.iterator())).noneMatch(fellow -> fellow.getRace().equals(Fellow.Race.GIANT));
+        assertThat(Streams.stream(fellowship.iterator()).count()).isEqualTo(9);
     }
 
     @Test
     public void shouldContainTupleForBoromirSamAndLegolas() {
-        // Extracting multiple values at once (using tuple to group them)
-        // Create tuples with name and race name
-        Triple<String, String, String> triple = Triple.of("Boromir", "Sam", "Legolas");
-        assertThat(fellowship.toString().contains(triple.toString()));
+//         Extracting multiple values at once (using tuple to group them)
+//         Create tuples with name and race name
+        List<Tuple> expectedTuples = new ArrayList<Tuple>() {
+            {
+                add(new Tuple(Fellow.Race.MAN, "Boromir"));
+                add(new Tuple(Fellow.Race.HOBBIT, "Sam"));
+                add(new Tuple(Fellow.Race.ELF, "Legolas"));
+            }
+        };
+
+        assertThat(fellowship).extracting(Fellow::getRace, Fellow::getName).containsAll(expectedTuples);
     }
 
 
     @Test
     public void shouldContainsFourHobbits() {
-        List<String> list = new ArrayList<>();
-        int numberOfHobbits = 0;
-        Iterator<Fellow> iterator = fellowship.iterator();
-        while(iterator.hasNext()) {
-            if(iterator.next().toString().contains("HOBBIT")) {
-                numberOfHobbits++;
-            }
-        }
-        assertThat(numberOfHobbits == 4);
+        assertThat(Streams.stream(fellowship.iterator())
+                .filter(fellow -> fellow.getRace() == Fellow.Race.HOBBIT))
+                .hasSize(4);
     }
 
 
